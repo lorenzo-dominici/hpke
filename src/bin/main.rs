a27b80383b8766d6287adf60e862042c045370b2
lorenzo-dominici <lorenzo.dominici@edu.unifi.it>
2023-11-23 14:20:58 +0100

    major fixes

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,26 +4,26 @@ use std::env;
 fn main() {
     let mut args: Vec<String> = env::args().skip(1).collect();
 
-    match args.len() {
-        0 => print_version(),
+    let str_out = match args.len() {
+        0 => version(),
         1 => match args[0].as_str() {
-            "--version" | "-v" => print_version(),
-            "--help" | "-h" => print_help(),
+            "--version" | "-v" => version(),
+            "--help" | "-h" => help(),
             _ => exit_println("Error: option not found"),
         },
         2.. => match args.remove(0).as_str() {
             "test" => if args.len() != 2 {
-                encrypt(args[0].as_str(), args[1].as_str());
+                test(args[0].as_str(), args[1].as_str(), args[2].as_str())
             } else {
                 exit_println("Error: wrong number of arguments");
             },
             "encrypt" => if args.len() != 2 {
-                encrypt(args[0].as_str(), args[1].as_str());
+                encrypt(args[0].as_str(), args[1].as_str())
             } else {
                 exit_println("Error: wrong number of arguments");
             },
             "decrypt" => if args.len() != 2 {
-                decrypt(args[0].as_str(), args[1].as_str());
+                decrypt(args[0].as_str(), args[1].as_str())
             } else {
                 exit_println("Error: wrong number of arguments");
             },
@@ -31,14 +31,15 @@ fn main() {
         },
         _ => exit_println("Error: I don't know what you did, but you fucked up!"),
     };
+    
+    print!("{}", str_out);
 }
 
-fn print_version() {
-    println!("hpke {}", option_env!("CARGO_PKG_VERSION").unwrap_or("unknown"))
+fn version() -> String {
+    format!("hpke_cl {}\n", option_env!("CARGO_PKG_VERSION").unwrap_or("unknown"))
 }
 
-fn print_help() {
-    let msg = 
+fn help() -> String {
 "HPKE command line tool
 
 Usage: hpke [OPTION] [COMMAND]
@@ -51,6 +52,5 @@ Commands:
     test <CONFIG> <DATA>        Test parameters in CONFIG with data in DATA
     encrypt <CONFIG> <DATA>     Encrypt plain-text in DATA with values in CONFIG
     decrypt <CONFIG> <DATA>     Dencrypt cypher-text in DATA with values in CONFIG
-";
-    println!("{}", msg);
+".to_string()
 }
